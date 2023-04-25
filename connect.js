4220fa8d331ed2a4c3cd8a95cc63a9b31521d1b9
MOHIT RANA <awesomemohit20@gmail.com>
2023-04-25 22:22:58 +0530

    added this file to connect to database

---
+++ connect.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const URI = process.env.MONGO_URI;
+console.log(URI);
+
+mongoose.connect(URI , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to database")
+}).catch((err) => {
+    console.error("Error connecting to database", err);
+});
+
+module.exports = mongoose.connection;
+
