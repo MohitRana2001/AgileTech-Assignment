d90aaf13115c7d7bffe8443f75782e90b9eba2f0
MOHIT RANA <awesomemohit20@gmail.com>
2023-04-26 16:04:21 +0530

    Added one more event

--- one-time-scripts/eventList.js
+++ one-time-scripts/eventList.js
@@ -6,18 +6,18 @@ async function start() {
     await mongoose.connect("mongodb+srv://mohit:1234@assignment.n16zdc6.mongodb.net/?retryWrites=true&w=majority");
     const eventInstance = new 
         event({
-            event: "Webinar",
-            uid: "11",
-            name: "Nodejs Best Practices",
-            tagline: "Learn nodeJs in best way.",
+            event: "Hackathon",
+            event_id: "23",
+            name: "ThinkTank Ideathon",
+            tagline: "Participate and win exciting goodies.",
             schedule: new Date("2023-05-01T15:00:00.000Z"),
-            description: "Join us for interactive session on nodeJs",
+            description: "Organized by AIML club",
             files: {
                 image: "/images/thinkTank.jpg",
             },
             moderator: "Mohit Rana",
             category: "Web Development",
-            sub_category: "NodeJs",
+            sub_category: "Hackathon",
             rigor_rank: 3,
             attendees: [{
                 name: "Mohit Rana",
